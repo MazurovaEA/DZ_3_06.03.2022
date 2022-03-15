4b808e497db329e75e1e2d854450d7baa2b38f1d
Katerina <ealmazurova@internet.ru>
2022-03-15 19:05:16 +0300

    Задача 3: вывод таблицы кубов введенного числа

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int N = Prompt("Введите N => ");
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(i * i * i + " ");
+}
